528500978da75e2f998723299bd241df9fcee298
Eugene <falcorrus@users.noreply.github.com>
2026-06-19 16:01:42 -0300

    fix(android): manually register SpeechRecognition plugin to prevent bridge hangs

--- android/app/src/main/java/com/reloto/coinlover/MainActivity.java
+++ android/app/src/main/java/com/reloto/coinlover/MainActivity.java
@@ -3,10 +3,12 @@ package com.reloto.coinlover;
 import android.os.Bundle;
 import android.webkit.PermissionRequest;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.speechrecognition.SpeechRecognition;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(SpeechRecognition.class);
         super.onCreate(savedInstanceState);
     }
 }
